fca8f18fd38b42a703fa7acb3baf35dc00e1dc9d
amanuelchorito <amye.end@gmail.com>
2021-09-21 17:48:03 -0500

    anagram

---
+++ src/arrayandstring/Anagram.java
@@ -0,0 +1,22 @@
+package arrayandstring;
+
+import java.util.Arrays;
+
+public class Anagram {
+    //implement is anagram
+    public static void main(String[] args) {
+        System.out.println(isAnagram("silent","listen"));
+    }
+
+    private static boolean isAnagram(String str1,String str2) {
+        char[]arr1= str1.toCharArray();
+        Arrays.sort(arr1);
+        char[]arr2=str2.toCharArray();
+        Arrays.sort(arr2);
+        String a= new String(arr1);
+        String b= new String(arr2);
+        if (a.equals(b))
+            return true;
+        return false;
+    }
+}
