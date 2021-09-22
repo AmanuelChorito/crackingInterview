c12f468056cb120c03a264a469a335eed164389b
amanuelchorito <amye.end@gmail.com>
2021-09-22 12:37:29 -0500

    final

--- src/grid/Anagram.java
+++ src/grid/Anagram.java
@@ -1,11 +1,14 @@
 package grid;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 
 public class Anagram {
     //implement is anagram
     public static void main(String[] args) {
-        System.out.println(isAnagram("silent","listen"));
+        System.out.println(anagramvWithHashMap("abc","cba"));
     }
 
     private static boolean isAnagram(String str1,String str2) {
@@ -19,10 +22,28 @@ public class Anagram {
             return true;
         return false;
     }
-    private static boolean anagramv2(String str1,String str2){
+    private static boolean anagramvWithHashMap(String str1,String str2){
+        if(str1.length()!=str2.length()) return false;
+        Map<Character,Integer> strHash1= new HashMap<>();
+        Map<Character,Integer>strHash2= new HashMap<>();
+        convertCharstringtoHashMap(str1, strHash1);
+        convertCharstringtoHashMap(str2, strHash2);
 
+        if(strHash1.equals(strHash2))
+            return true;
         return false;
 
+    }
+    public static Map<Character,Integer>convertCharstringtoHashMap(String str,Map<Character,Integer> strHash){
+        for(int i=0;i<str.length();i++){
+            if( strHash.containsKey(str.charAt(i))){
+                Integer v= strHash.get(str.charAt(i));
+                strHash.replace(str.charAt(i),v+1);
+
+            }
+            strHash.put(str.charAt(i),1);
+        }
+        return strHash;
     }
 
 }
--- src/grid/ImmutableClass.java
+++ src/grid/ImmutableClass.java
@@ -1,4 +1,34 @@
 package grid;
 
-public class ImmutableClass {
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
+public final class ImmutableClass {
+    ////once object is created, we can't change its content
+    //string class, warapper classes are immutable
+    //final class
+    //private memebr
+    //deep copy
+
+    private final String name;
+    private final int regNo;
+    private final Map<String, String> metadata;
+    public ImmutableClass(String name, int regNo, Map<String, String> metadata) {
+        this.name = name;
+        this.regNo = regNo;
+        this.metadata = metadata;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRegNo() {
+        return regNo;
+    }
+
+    public Map<String, String> getMetadata() {
+        return Collections.unmodifiableMap( metadata);
+    }
 }
